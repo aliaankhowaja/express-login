fc75e4e2ea7139cb0ca5cd0fa6644f3afadabb3d
aliaankhowaja <aliaankhowaja@gmail.com>
2026-03-06 16:16:44 +0500

    feat: implemented logout functionality

--- routes/users.js
+++ routes/users.js
@@ -25,4 +25,14 @@ router.get('/dashboard', (req, res) => {
     }
 });
 
+// logout route
+router.post('/logout', (req, res) => {
+    req.session.destroy(err => {
+        if (err) {
+            return res.status(500).json({ message: 'Logout failed' });
+        }
+        res.json({ message: 'Logout successful' });
+    });
+});
+
 module.exports = router;
